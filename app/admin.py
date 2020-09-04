197b7f4ccbe68852c9e0c8a9fa432af69caa6e72
ichimichi <ichimichilaribok@gmail.com>
2020-09-05 00:00:52 +0530

    models update

--- app/admin.py
+++ app/admin.py
@@ -1,23 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Employee, Item, Transaction, TransactionDetails
+
+from app.models import Employee, Item, Transaction, TransactionDetails, Department
+
 
 class TransactionDetailsInline(admin.TabularInline):
     model = TransactionDetails
-    extra = 3
+    extra = 1
 
 class TransactionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Transaction Details', {'fields': ['type']}),
         ('Date Information', {'fields':['date']}),
-        ('Employee Details', {'fields':['employee']}),
+        ('Department Details', {'fields':['department']}),
     ]
     inlines = [TransactionDetailsInline]
+    list_display = ['date', 'type', 'department']
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age', 'department']
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'name', 'quantity']
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'num_emp']
 
 
-admin.site.register(Employee)
-admin.site.register(Item)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Transaction, TransactionAdmin)
 
 
--- app/models.py
+++ app/models.py
@@ -1,38 +1,80 @@
 from django.db import models
 
 # Create your models here.
-class Employee(models.Model):
-    # emp_id = models.CharField(max_length=10, primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return str(self.first_name)+' '+str(self.last_name)
-
-class Item(models.Model):
-    # item_id = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
-
-    def __str__(self):
-        return str(self.brand)+' '+str(self.name)
-
+DEPARTMENT_TYPE = (
+    ('CSE', 'Computer Science Engineering'),
+    ('ECE', 'Electronic Communication Engineering'),
+    ('EEE', 'Electronical Electronic Engineering'),
+    ('CE', 'Civil Engineering'),
+    ('ME', 'Mechanical Engineering'),
+    ('WH', 'Ware House')
+)
 
 TRANSACTION_TYPE = (
    ('I', 'Incoming'),
    ('O', 'Outoing')
 )
 
+class Department(models.Model):
+    name = models.CharField(choices=DEPARTMENT_TYPE, max_length=128, primary_key=True)
+
+    def num_emp(self):
+        return Employee.objects.filter(department = self.name).count()
+    num_emp.short_description = 'Number of Employees'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    age = models.IntegerField(default=0)
+    department = models.ForeignKey(Department, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return str(self.first_name)+' '+str(self.last_name)
+
+
+class Item(models.Model):
+    # item_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=64)
+    brand = models.CharField(max_length=64)
+
+    def quantity(self):
+        in_sum = 0
+        out_sum = 0
+        incoming = Transaction.objects.filter(type='I')
+        outgoing = Transaction.objects.filter(type='O')
+        for t in outgoing:
+            try:
+                temp = TransactionDetails.objects.filter(transaction=t, item=self.id).values_list('quanity', flat=True)[0]
+            except:
+                temp = 0
+            out_sum += temp
+        for t in incoming:
+            try:
+                temp = TransactionDetails.objects.filter(transaction=t, item=self.id).values_list('quanity', flat=True)[0]
+            except:
+                temp = 0
+            in_sum += temp
+        return in_sum-out_sum
+
+    def __str__(self):
+        return str(self.brand)+' '+str(self.name)
+
+
 class Transaction(models.Model):
-    employee = models.ForeignKey(Employee, on_delete= models.DO_NOTHING)
+    department = models.ForeignKey(Department, on_delete= models.DO_NOTHING)
     type = models.CharField(choices=TRANSACTION_TYPE, max_length=128)
     date = models.DateTimeField()
+
 
 class TransactionDetails(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete= models.DO_NOTHING)
-    quanity = models.IntegerField(default=0)
+
+    quanity = models.IntegerField(default=1)
 
 
 
